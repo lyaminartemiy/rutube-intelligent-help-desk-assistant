30cb3f250163cb6f58f71fffc8b91fd33cb7f0c0
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-01 21:01:06 +0300

    feature: created backend data model

---
+++ backend/app/src/main/java/org/example/User.java
@@ -0,0 +1,74 @@
+package org.example;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    Long id;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    Role role;
+
+    String fullName;
+
+    String shortName;
+
+    String username;
+
+    String password;
+
+    @ManyToMany
+    List<Session> sessions;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.toString()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+    public enum Role {
+        BOT,
+        TECHNICAL_SUPPORT,
+        ADMIN
+    }
+}
