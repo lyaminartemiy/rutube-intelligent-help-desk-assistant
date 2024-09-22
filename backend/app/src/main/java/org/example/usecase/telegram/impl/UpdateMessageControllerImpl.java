3ec40cf89dc375d671c066f756375232009e9fb4
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-22 17:50:37 +0300

    Первые тесты с Артемом

--- backend/app/src/main/java/org/example/usecase/telegram/impl/UpdateMessageControllerImpl.java
+++ backend/app/src/main/java/org/example/usecase/telegram/impl/UpdateMessageControllerImpl.java
@@ -3,7 +3,7 @@ package org.example.usecase.telegram.impl;
 import lombok.RequiredArgsConstructor;
 import org.example.model.dto.UpdateMessageDto;
 import org.example.usecase.telegram.UpdateMessageController;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +16,7 @@ public class UpdateMessageControllerImpl implements UpdateMessageController {
     private final UpdateMessageService service;
 
     @Override
-    @PutMapping
+    @PostMapping
     public void updateMessageInDatabase(@RequestBody UpdateMessageDto requestBody) {
         service.createOrUpdateMessageInDatabase(
                 requestBody.chatId(),
