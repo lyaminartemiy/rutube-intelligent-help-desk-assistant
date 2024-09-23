2559a5e8105d29b0fa189dc2b7d2a545024e523a
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-23 22:46:24 +0300

    фикс бека

--- backend/app/src/main/java/org/example/model/dto/AiMessageDto.java
+++ backend/app/src/main/java/org/example/model/dto/AiMessageDto.java
@@ -1,10 +1,13 @@
 package org.example.model.dto;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.annotation.Nullable;
 import org.example.model.entity.Message;
 
 import java.time.ZonedDateTime;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record AiMessageDto(
         String text,
         ZonedDateTime createdAt,
