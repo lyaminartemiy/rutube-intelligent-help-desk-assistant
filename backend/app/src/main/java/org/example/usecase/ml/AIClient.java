5341f01a3008ff7104411f2868ceccb51d5cc402
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-23 23:54:38 +0300

    фикс бека

--- backend/app/src/main/java/org/example/usecase/ml/AIClient.java
+++ backend/app/src/main/java/org/example/usecase/ml/AIClient.java
@@ -3,7 +3,7 @@ package org.example.usecase.ml;
 import org.example.model.dto.AiMessageDto;
 import org.example.model.dto.AiResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -11,6 +11,6 @@ import java.util.List;
 @FeignClient(name = "aiClient", url = "http://hackaton-ml:8001")
 public interface AIClient {
 
-    @GetMapping("/api/qa")
+    @PostMapping("/api/qa")
     AiResponse getAiResponse(@RequestBody List<AiMessageDto> requestBody);
 }
