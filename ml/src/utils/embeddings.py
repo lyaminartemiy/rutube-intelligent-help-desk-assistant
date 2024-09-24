7dd447f3c74d48f6735a5e6483ce3d3ae11f0b6c
Lyamin Artemiy <adlyamin@sberbank.ru>
2024-09-24 16:05:34 +0300

    Minor upgrades

--- ml/src/utils/embeddings.py
+++ ml/src/utils/embeddings.py
@@ -3,6 +3,8 @@ import os
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
+from langchain.schema import Document
+# from resources.messages import data
 
 
 def generate_embeddings(embeddings_model: OpenAIEmbeddings):
@@ -15,3 +17,19 @@ def generate_embeddings(embeddings_model: OpenAIEmbeddings):
     texts = text_splitter.create_documents(pages)
     db = Chroma.from_documents(texts, embeddings_model, persist_directory="./resources/chroma_db")
     print("persist_directory:", db._persist_directory)
+
+
+# def generate_embeddings(embeddings_model: OpenAIEmbeddings):
+#     # Создание списка документов из данных
+#     texts = []
+#     for section, qa_pairs in data.items():
+#         for question, answer in qa_pairs.items():
+#             # Создаем документ для каждого вопроса и ответа
+#             text_content = f"Вопрос: {question}\nОтвет: {answer}"
+#             texts.append(Document(page_content=text_content))
+    
+#     for i,text in enumerate(texts):
+#         print("i:", i, "page_content:", text.page_content)
+
+#     # Создание эмбеддингов и добавление в ChromaDB
+#     db = Chroma.from_documents(texts, embeddings_model, persist_directory="./resources/chroma_db")
--- ml/src/utils/qa_system.py
+++ ml/src/utils/qa_system.py
@@ -19,10 +19,24 @@ def rerank_documents(info, k: int, cross_encoder: CrossEncoder) -> List[Tuple[An
             doc_score = doc['score']
             top_k.append((docs[doc_id], doc_score))
         return top_k
+    if not info['docs_context']:
+        print("НЕТ КОНТЕКСТА!")
+        return info
 
     question = info['question']
+    # docs_context = [doc.page_content for doc in info['docs_context']]
     docs_context = [doc.page_content for doc in info['docs_context']]
+    for i, docs in enumerate(docs_context):
+        print(f"Документ {i}: {docs}")
+    
+    for docs in docs_context:
+        print("Новый документ", docs)
+
     docs_rank_result = cross_encoder.rank(question, docs_context)
+    print("Результаты ранжирования:", docs_rank_result)
+
     docs_res = get_top_k(docs_rank_result, docs_context)
+    print("Результаты получение топ-К:", docs_res)
+    
     info['docs_context'] = docs_res
     return info
