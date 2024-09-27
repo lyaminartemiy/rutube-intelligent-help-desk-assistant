f04e5c2c929925bfed22ab86580ac300099222aa
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-27 23:56:25 +0300

    ПОЕХАЛИ НОВЫЙ БЭК И ФРОНТ

--- backend/app/src/main/java/org/example/model/entity/TechSupportRequest.java
+++ backend/app/src/main/java/org/example/model/entity/TechSupportRequest.java
@@ -8,7 +8,6 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -40,8 +39,7 @@ public class TechSupportRequest {
 
     String title;
 
-    @OneToOne
-    @JoinColumn(name = "session_id")
+    @OneToOne(mappedBy = "request")
     Session session;
 
     @Enumerated(EnumType.STRING)
