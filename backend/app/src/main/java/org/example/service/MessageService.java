be20e255efc3373667a584cdc53899f44562149f
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-28 01:55:09 +0300

    ПОЕХАЛИ НОВЫЙ БЭК И ФРОНТ

--- backend/app/src/main/java/org/example/service/MessageService.java
+++ backend/app/src/main/java/org/example/service/MessageService.java
@@ -3,8 +3,12 @@ package org.example.service;
 
 import lombok.RequiredArgsConstructor;
 import org.example.model.dto.MessageDto;
+import org.example.model.entity.Message;
+import org.example.model.entity.Session;
 import org.example.model.entity.TechSupportRequest;
 import org.example.repository.TechSupportRequestRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,12 +17,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MessageService {
 
+    private static final Logger log = LoggerFactory.getLogger(MessageService.class);
     private final TechSupportRequestRepository techSupportRequestRepository;
 
     public List<MessageDto> getMessagesByRequestId(Long requestId) {
-        TechSupportRequest request = techSupportRequestRepository.findById(requestId).orElse(null);
+        log.info("1");
+        TechSupportRequest request = techSupportRequestRepository.findById(requestId).orElseThrow(() -> new IllegalStateException("ПИЗДЕЦ"));
+        log.info("2");
         assert request != null;
-        return request.getSession().getMessages().stream().map(
+        log.info("3");
+        Session requestSession = request.getSession();
+        log.info("4");
+        List<Message> messages = requestSession.getMessages();
+        log.info("5");
+        return messages.stream().map(
                 m -> new MessageDto(
                         m.getMessageText(),
                         m.getCreatedAt(),
