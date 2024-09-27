9b8dc0f0d3b8461c35914128f63f285fe7776380
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-27 23:40:48 +0300

    ПОЕХАЛИ НОВЫЙ БЭК И ФРОНТ

--- backend/app/src/main/java/org/example/service/MessageService.java
+++ backend/app/src/main/java/org/example/service/MessageService.java
@@ -5,12 +5,14 @@ import lombok.RequiredArgsConstructor;
 import org.example.model.dto.MessageDto;
 import org.example.model.entity.TechSupportRequest;
 import org.example.repository.MessageRepository;
+import org.example.repository.SessionRepository;
 import org.example.repository.TechSupportRequestRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +21,11 @@ public class MessageService {
     private static final Logger log = LoggerFactory.getLogger(MessageService.class);
     private final MessageRepository messageRepository;
     private final TechSupportRequestRepository techSupportRequestRepository;
+    private final SessionRepository sessionRepository;
 
     public List<MessageDto> getMessagesByRequestId(Long requestId) {
         log.info("getMessagesByRequestId: {}", requestId);
-        TechSupportRequest request = techSupportRequestRepository.findById(requestId).get();
+        TechSupportRequest request = sessionRepository.findAll().stream().filter(s -> Objects.equals(s.getRequest().getId(), requestId)).toList().getFirst().getRequest();
         return messageRepository.findBySession(request.getSession()).stream()
                 .map(m -> new MessageDto(
                         m.getMessageText(),
