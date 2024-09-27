28e64abf71a068b4af3ed2b93299e110fc3212b6
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-27 16:47:35 +0300

    фикс  бека

--- backend/app/src/main/java/org/example/App.java
+++ backend/app/src/main/java/org/example/App.java
@@ -4,19 +4,11 @@
 package org.example;
 
 import org.example.model.entity.Employee;
-import org.example.repository.EmployeeRepository;
-import org.example.repository.TechSupportRequestRepository;
 import org.example.service.AuthService;
-import org.example.service.TechSupportRequestService;
-import org.example.usecase.telegram.impl.SessionService;
-import org.example.usecase.telegram.impl.UpdateMessageService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.ConfigurableApplicationContext;
-
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -29,54 +21,54 @@ public class App {
                 Employee.Role.ADMIN,
                 "olenkaevstratova@gmail.com"
         );
-        ctx.getBean(AuthService.class).sendSignUpDataToEmail(
-                "Петров Виктор Администраторович",
-                Employee.Role.TECH_SUPPORT_EMPLOYEE,
-                "olenkaevstratova@gmail.com"
-        );
-        ctx.getBean(AuthService.class).sendSignUpDataToEmail(
-                "Петров Виктор Администраторович",
-                Employee.Role.TECH_SUPPORT_EMPLOYEE,
-                "olenkaevstratova@gmail.com"
-        );
+//        ctx.getBean(AuthService.class).sendSignUpDataToEmail(
+//                "Петров Виктор Администраторович",
+//                Employee.Role.TECH_SUPPORT_EMPLOYEE,
+//                "olenkaevstratova@gmail.com"
+//        );
+//        ctx.getBean(AuthService.class).sendSignUpDataToEmail(
+//                "Петров Виктор Администраторович",
+//                Employee.Role.TECH_SUPPORT_EMPLOYEE,
+//                "olenkaevstratova@gmail.com"
+//        );
 
-        var sessionService = ctx.getBean(SessionService.class);
-        sessionService.createNewSession("123");
-        sessionService.createNewSession("456");
-        sessionService.createNewSession("789");
-        sessionService.createNewSession("0123");
-        sessionService.sendSessionToTechSupport("0123");
-        sessionService.sendSessionToTechSupport("123");
-        sessionService.sendSessionToTechSupport("456");
-        sessionService.sendSessionToTechSupport("789");
-
-        var updateMessageService = ctx.getBean(UpdateMessageService.class);
-        updateMessageService.createOrUpdateMessageInDatabase(
-                "123",
-                "fake-message-id1",
-                "Спасите помогите я такой-то такой-то",
-                ZonedDateTime.now(ZoneOffset.UTC),
-                null
-        );
-        updateMessageService.createOrUpdateMessageInDatabase(
-                "456",
-                "fake-message-id2",
-                "Спасите помогите я такой-то такой-то",
-                ZonedDateTime.now(ZoneOffset.UTC),
-                null
-        );
-        updateMessageService.createOrUpdateMessageInDatabase(
-                "789",
-                "fake-message-id3",
-                "Спасите помогите я такой-то такой-то",
-                ZonedDateTime.now(ZoneOffset.UTC),
-                null
-        );
-        var techSupRqService = ctx.getBean(TechSupportRequestService.class);
-        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getFirst().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getFirst());
-        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getFirst().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getLast());
-        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getLast().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getFirst());
-        techSupRqService.closeRequestById(ctx.getBean(TechSupportRequestRepository.class).findAll().stream().filter(rq -> rq.getSession().getChatId().equals("0123")).toList().getLast().getId());
+//        var sessionService = ctx.getBean(SessionService.class);
+//        sessionService.createNewSession("123");
+//        sessionService.createNewSession("456");
+//        sessionService.createNewSession("789");
+//        sessionService.createNewSession("0123");
+//        sessionService.sendSessionToTechSupport("0123");
+//        sessionService.sendSessionToTechSupport("123");
+//        sessionService.sendSessionToTechSupport("456");
+//        sessionService.sendSessionToTechSupport("789");
+//
+//        var updateMessageService = ctx.getBean(UpdateMessageService.class);
+//        updateMessageService.createOrUpdateMessageInDatabase(
+//                "123",
+//                "fake-message-id1",
+//                "Спасите помогите я такой-то такой-то",
+//                ZonedDateTime.now(ZoneOffset.UTC),
+//                null
+//        );
+//        updateMessageService.createOrUpdateMessageInDatabase(
+//                "456",
+//                "fake-message-id2",
+//                "Спасите помогите я такой-то такой-то",
+//                ZonedDateTime.now(ZoneOffset.UTC),
+//                null
+//        );
+//        updateMessageService.createOrUpdateMessageInDatabase(
+//                "789",
+//                "fake-message-id3",
+//                "Спасите помогите я такой-то такой-то",
+//                ZonedDateTime.now(ZoneOffset.UTC),
+//                null
+//        );
+//        var techSupRqService = ctx.getBean(TechSupportRequestService.class);
+//        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getFirst().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getFirst());
+//        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getFirst().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getLast());
+//        techSupRqService.assignEmployeeToRequest(ctx.getBean(TechSupportRequestRepository.class).findAll().getLast().getId(), ctx.getBean(EmployeeRepository.class).findAll().stream().filter(e -> e.getRole().equals(Employee.Role.TECH_SUPPORT_EMPLOYEE)).toList().getFirst());
+//        techSupRqService.closeRequestById(ctx.getBean(TechSupportRequestRepository.class).findAll().stream().filter(rq -> rq.getSession().getChatId().equals("0123")).toList().getLast().getId());
 
 
 //        Thread.sleep(20000);
