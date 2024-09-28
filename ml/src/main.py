a81064c7021370846d629d891218ec618e53eea5
lexa2hk <alesha.smolnikoff@gmail.com>
2024-09-28 14:38:11 +0300

    ml test

--- ml/src/main.py
+++ ml/src/main.py
@@ -4,10 +4,14 @@ from fastapi import FastAPI
 from lifespan import lifespan
 from routers.qa_system import router as qa_system_router
 
+from tensorflow.python.client import device_lib
+
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(qa_system_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    print("ML")
+    device_lib.list_local_devices()
