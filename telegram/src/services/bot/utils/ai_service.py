50e1d886990812a782db570bb4ed75ef52bb4b51
Lyamin Artemiy <adlyamin@sberbank.ru>
2024-09-28 22:39:15 +0300

    fix

--- telegram/src/services/bot/utils/ai_service.py
+++ telegram/src/services/bot/utils/ai_service.py
@@ -2,16 +2,12 @@ from typing import Union, Dict, Any
 
 import aiohttp
 
-from config import config
-
 
 async def post_question_in_ai_service(question: str) -> None:
     """Log an event to the AI Service."""
-    url = config.AIServiceSettings.BASE_URL + config.AIServiceSettings.QUESTION_ENDPOINT
-    print("url: ", url)
     async with aiohttp.ClientSession() as session:
         async with session.post(
-            url=url,
+            url="http://176.109.100.31:8001/predict",
             json={"question": question},
             headers={"Content-Type": "application/json"},
         ) as response:
