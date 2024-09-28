c3b8ddb1e7017bcfc61bff06204bee32a8c946c7
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-28 14:29:53 +0300

    фиксы бека

--- backend/app/src/main/java/org/example/service/TechSupportRequestService.java
+++ backend/app/src/main/java/org/example/service/TechSupportRequestService.java
@@ -10,6 +10,8 @@ import org.example.repository.EmployeeRepository;
 import org.example.repository.SessionRepository;
 import org.example.repository.TechSupportRequestRepository;
 import org.example.usecase.telegram.impl.SendMessageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,6 +20,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TechSupportRequestService {
 
+    private static final Logger log = LoggerFactory.getLogger(TechSupportRequestService.class);
     private final TechSupportRequestRepository repository;
     private final SessionRepository sessionRepository;
     private final TechSupportRequestRepository techSupportRequestRepository;
@@ -48,7 +51,8 @@ public class TechSupportRequestService {
 
     public void assignEmployeeToRequest(Long requestId, Employee employee) {
         TechSupportRequest request = techSupportRequestRepository.findById(requestId).get();
-        if (!request.getAssignedEmployees().contains(employee)) {
+        if (!request.getAssignedEmployees().stream().map(Employee::getId).toList().contains(employee.getId())) {
+            log.info("Assign employee with id {} to request id {}", employee.getId(), requestId);
             request.getAssignedEmployees().add(employee);
             request.setStatus(TechSupportRequest.Status.IN_PROGRESS);
             List<TechSupportRequest> requestsInProgress = employee.getRequestsInProgress();
