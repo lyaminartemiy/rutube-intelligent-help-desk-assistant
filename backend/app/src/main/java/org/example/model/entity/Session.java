8b782fc5c1a5459ae84994ea6b7688b739d8d7a0
KLINY1337 <semyon.chernomurov@gmail.com>
2024-09-28 23:18:13 +0300

    фиксы бека

--- backend/app/src/main/java/org/example/model/entity/Session.java
+++ backend/app/src/main/java/org/example/model/entity/Session.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -53,7 +54,7 @@ public class Session {
     @OneToOne(mappedBy = "session")
     TechSupportRequest request;
 
-    @OneToMany(mappedBy = "session")
+    @OneToMany(mappedBy = "session", fetch = FetchType.LAZY)
     List<Message> messages;
 
     public enum Status {
